4dbd3214b6a869d929e4ebc2a1f10f351a59dc33
Alejandro Berrio <161344955+BerrioA@users.noreply.github.com>
2026-03-20 23:16:16 -0500

    Import dns and set default result order to ipv4first

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import dns from "dns";
+dns.setDefaultResultOrder("ipv4first");
+
 import app from "./app.js";
 import { sequelize } from "./config/db.js";
 import { NODE_ENV, PORT } from "./config/env.js";
